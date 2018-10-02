bcb4b4c021cfd8d2facacef77d23cc38f763aeed
pgossa <pierre.gossa@gmail.com>
2018-10-02 21:44:29 +0200

    Add files via upload

---
+++ extractor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+listid=$(tail -n +2 idmovie.csv)
+for i in $listid
+do
+	imdbid=$(echo $i | cut -d',' -f2)
+	tmdbid=$(echo $i | cut -d',' -f3)
+	if [[ $tmdbid = *[^[:space:]]* ]] 
+	then 
+		currentURL="https://www.themoviedb.org/movie/$tmdbid"
+		content=$(wget -q -O - $currentURL | xmllint --html --xpath '//div[@class = "overview"]' - 2>/dev/null | tail -n +2 | head -n +2 | sed 's#<p>##g' | sed 's#</p>##g')
+	else 
+		currentURL="https://www.imdb.com/title/tt$imdbid/"
+	content=$(wget -q -O - $currentURL | xmllint --html --xpath '//div[@class = "inline canwrap"]' - 2>/dev/null | xmllint --html --xpath '//span' - 2>/dev/null | cut  -c11- | sed 's#</span>##g')
+	fi
+	echo $content
+done
