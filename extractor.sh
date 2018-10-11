cdec414b8c2c46ee901d5d538c045cb75757c928
Mathieu <mamat489@users.noreply.github.com>
2018-10-11 11:50:03 +0200

    add "remove punctuation function"

--- extractor.sh
+++ extractor.sh
@@ -10,13 +10,17 @@ do
 	then 
 		currentURL="https://www.themoviedb.org/movie/$tmdbid"
 		content=$(wget -q -O - $currentURL | xmllint --html --xpath '//div[@class = "overview"]' - 2>/dev/null | tail -n +2 | head -n +2 | cut  -c19- | sed 's#<p>##g' | sed 's#</p>##g' | sed "s#'#''#g" | tr -d '\n')
+		content2=$(echo $content | sed "s#[[:punct:]]# #g")
+
 	else 
 		currentURL="https://www.imdb.com/title/tt$imdbid/"
-	content=$(wget -q -O - $currentURL | xmllint --html --xpath '//div[@class = "inline canwrap"]' - 2>/dev/null | xmllint --html --xpath '//span' - 2>/dev/null | cut  -c11- | sed 's#</span>##g' | sed "s#'#''#g")
+		content=$(wget -q -O - $currentURL | xmllint --html --xpath '//div[@class = "inline canwrap"]' - 2>/dev/null | xmllint --html --xpath '//span' - 2>/dev/null | cut  -c11- | sed 's#</span>##g' | sed "s#'#''#g")
+		content2=$(echo $content | sed "s#[[:punct:]]# #g")
 	fi
 	echo $content
 	echo $bddid
 	export LD_LIBRARY_PATH=./lib/
 	./bin/psql -h 127.0.0.1 -p 5454 -U cinephile -d cinema -c "BEGIN;UPDATE film SET synopsis = '$content' WHERE id_film=$bddid;COMMIT;"
+	./bin/psql -h 127.0.0.1 -p 5454 -U cinephile -d cinema -c "BEGIN;UPDATE film SET vector = '$content2' WHERE id_film=$bddid;COMMIT;"
 	#psql -h 127.0.0.1 -p 5454 -U cinephile -d cinema -c "BEGIN;UPDATE film SET synopsis = '$content' WHERE id_film=$bddid;COMMIT;"
 done
